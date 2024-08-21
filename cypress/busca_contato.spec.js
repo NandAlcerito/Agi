76e3878c00b33ab1dea0afdebf3a65a8c9dbc959
NandAlcerito <fernandamca@icloud.com>
2024-08-21 17:20:18 -0300

    Update busca_contato.spec.js

--- cypress/busca_contato.spec.js
+++ cypress/busca_contato.spec.js
@@ -1,7 +1,7 @@
 // cypress/integration/busca_contato.spec.js
 
 describe('Busca pela palavra "contato"', () => {
-    it('O primeiro resultado deve redirecionar para https://agibank.com.br', () => {
+    it('O primeiro resultado deve ser um artigo com link para https://agibank.com.br', () => {
         // Passo 1: Acessar a página principal do site.
         cy.visit('https://blogdoagi.com.br');
 
@@ -13,19 +13,15 @@ describe('Busca pela palavra "contato"', () => {
         // Passo 3: Verificar o primeiro resultado.
         cy.get('.post') // Seletor para os resultados da busca
             .first() // Seleciona o primeiro artigo
-            .should('contain', 'Contato') // Verifica que o título contém a palavra "Contato"
+            .should('exist') // Verifica se o artigo existe
             .find('a') // Seleciona o link dentro do primeiro resultado
+            .should('have.attr', 'href', 'https://agibank.com.br') // Verifica se o link é o correto
             .then(($link) => {
-                // Passo 4: Verifica se o link leva ao URL correto
-                const url = $link.prop('href'); // Pega o atributo href do link
-                expect(url).to.eq('https://agibank.com.br'); // Verifica se o link é o correto
-
-                // Passo 5: Clicar no link e verificar o redirecionamento
+                // Passo 4: Clicar no link e verificar o redirecionamento
                 cy.wrap($link).click(); // Clica no link
             });
 
-        // Passo 6: Verificar se foi redirecionado corretamente.
+        // Passo 5: Verificar se foi redirecionado corretamente.
         cy.url().should('eq', 'https://agibank.com.br'); // Verifica se a URL atual é a correta
     });
 });
-
